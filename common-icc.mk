566f6cb2eacdfa51de2d18abeb6a995a0930975a
Andreas Prell <andreas.prell@uni-bayreuth.de>
2013-12-05 13:48:57 +0100

    Compile with icc -O2 instead of -O3

--- common-icc.mk
+++ common-icc.mk
@@ -1,7 +1,7 @@
 # Common definitions
 CC = icc
 CPPFLAGS += -D_GNU_SOURCE 
-CFLAGS += -O3 -g -Wall -Wcheck -mmic $(INCLUDE)
+CFLAGS += -O2 -Wall -Wcheck -mmic $(INCLUDE)
 LDFLAGS += -mmic
 
 INCLUDE = -I. -I../include
