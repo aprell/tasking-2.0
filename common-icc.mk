032f49f46a15f90f162e05a4b85ee87b4b1b4f3d
Andreas Prell <andreas.prell@uni-bayreuth.de>
2013-12-18 16:45:18 +0100

    Disable some warnings when compiling with icc

--- common-icc.mk
+++ common-icc.mk
@@ -1,7 +1,14 @@
 # Common definitions
 CC = icc
 CPPFLAGS += -D_GNU_SOURCE 
-CFLAGS += -O2 -Wall -Wcheck -mmic $(INCLUDE)
+CFLAGS += -O2 -Wall -Wcheck $(DISABLE) -mmic $(INCLUDE)
 LDFLAGS += -mmic
+
+# icc generates lots of remarks and warnings, especially when compiling UTS
+# Disable the following:
+# Warning  #161: unrecognized #pragma
+# Warning #2259: non-pointer conversion [...] may lose significant bits
+# Warning #3180: unrecognized OpenMP #pragma
+DISABLE = -wd161,2259,3180
 
 INCLUDE = -I. -I../include
