d9eaa49427513b2d2934f63959ad4bd0d6e631b1
Andreas Prell <andreas.prell@uni-bayreuth.de>
2017-01-31 17:05:36 +0100

    Suppress warnings about unused functions (GCC)

--- common-gcc.mk
+++ common-gcc.mk
@@ -1,6 +1,8 @@
 # Common definitions
 CC = gcc
 CPPFLAGS += -D_GNU_SOURCE
-CFLAGS += -O3 -Wall -Wextra $(INCLUDE)
+CFLAGS += -O3 -Wall -Wextra $(addprefix -Wno-,$(DISABLE)) $(INCLUDE)
+
+DISABLE = unused-function
 
 INCLUDE = -I. -I../include
