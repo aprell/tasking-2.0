aprell/tasking-2.0

commit 18deefb58044b666c45aa0e8960778c4482f9976

Support runtime system reinitialization after shutdown
FIXME: Repeatedly binding thread 0 to the same CPU seems to cause a
problem.

src/tasking_internal.c
@@ -78,6 +78,7 @@ int tasking_internal_init(int *argc UNUSED, char ***argv UNUSED)
 	ID = IDs[0] = 0;
 
 	// Bind master thread to CPU 0
+	// XXX Calling this more than once results in a significant slowdown! Why?
 	set_thread_affinity(0);
 
 	// Create num_workers-1 worker threads

src/runtime.c
@@ -318,9 +318,23 @@ int RT_init(void)
 		}
 	}
 
-	steal_req.ID = ID;
-	steal_req.partition = my_partition->number;
-	steal_req.pID = pID;
+	steal_req = (struct steal_request){
+		.ID = ID,
+		.try = 0,
+		.pass = 0,
+		.partition = my_partition->number,
+		.pID = pID,
+#ifdef STEAL_BACKOFF
+		.rounds = 0,
+#endif
+		.idle = false,
+		.quiescent = false
+#ifdef STEAL_ADAPTIVE
+		, .stealhalf = false
+#endif
+	};
+
+	requested = false;
 
 	timer_new(&timer_run_tasks, CPUFREQ);
 	timer_new(&timer_enq_deq_tasks, CPUFREQ);
@@ -350,6 +364,8 @@ int RT_exit(void)
 	MASTER {
 		channel_free(chan_barrier);
 	}
+
+	PARTITION_RESET();
 
 	return 0;
 }

src/runtime.h
@@ -56,6 +56,14 @@ static void PARTITION_SET(void) \
 	} \
 } \
 
+// Requires that PARTITION_INIT() was called
+#define PARTITION_RESET() \
+	num_partitions = 0; \
+	my_partition = NULL; \
+	is_manager = false; \
+	next_manager = 0; \
+	next_worker = 0;
+
 // Create a partition of n workers with identifier id
 // For example:
 // PARTITION_CREATE(abc, 4) = { 2, 4, 6, 8 };

commit 6699cff8a22131db4177c5624cc520744e1df28e

Fix performance problem introduced by commit 18deefb
The function cpu_count quietly assumes that the calling thread has
unrestricted affinity. To work around this limitation, we call
cpu_count() only once at the beginning and never after changing thread
0's affinity. Because thread 0 is bound to run on processor 0,
subsequent invocations of cpu_count() would return 1, and every thread
created by thread 0 would end up being pinned to processor 0. This
happened previously, causing the performance problem.

src/affinity.h
@@ -42,6 +42,7 @@ static inline int cpu_count(void)
 {
 	cpu_set_t cpuset;
 
+	// Requires unrestricted affinity!
 	pthread_getaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
 
 	return CPU_COUNT(&cpuset);

src/tasking_internal.c
@@ -36,6 +36,7 @@ static void *worker_entry_fn(void *args)
 	ID = *(int *)args;
 	set_current_task(NULL);
 	num_tasks_exec = 0;
+	tasking_finished = false;
 
 	RT_init();
 	tasking_internal_barrier();
@@ -52,8 +53,10 @@ static void *worker_entry_fn(void *args)
 
 int tasking_internal_init(int *argc UNUSED, char ***argv UNUSED)
 {
+	static int num_cpus;
+
 	char *envval;
-	int num_cpus, i;
+	int i;
 
 	envval = getenv("NUM_THREADS");
 	if (envval) {
@@ -63,7 +66,10 @@ int tasking_internal_init(int *argc UNUSED, char ***argv UNUSED)
 		num_workers = sysconf(_SC_NPROCESSORS_ONLN);
 	}
 
-	num_cpus = cpu_count();
+	// Call cpu_count() only once, before changing the affinity of thread 0!
+	// After set_thread_affinity(0), cpu_count() would return 1, and every
+	// thread would end up being pinned to processor 0.
+	num_cpus = (num_cpus == 0) ? cpu_count() : num_cpus;
 	printf("Number of CPUs: %d\n", num_cpus);
 
 	// Beware of false sharing!
@@ -113,6 +119,7 @@ int tasking_internal_init(int *argc UNUSED, char ***argv UNUSED)
 	current_task->sst = 0;
 
 	num_tasks_exec = 0;
+	tasking_finished = false;
 
 	return 0;
 }