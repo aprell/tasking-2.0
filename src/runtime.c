c353f1e91bd611388a305791dfbae1be9a2e3d09
Andreas Prell <andreas.prell@uni-bayreuth.de>
2018-11-02 17:25:51 +0100

    Update comments

--- src/runtime.c
+++ src/runtime.c
@@ -63,27 +63,27 @@ static struct task_indicator task_indicators[MAXWORKERS];
 /*
  * When a steal request is returned to its sender after MAX_STEAL_ATTEMPTS
  * unsuccessful attempts, the steal request changes state to STATE_FAILED and
- * is then passed on to tree.parent, which holds on to this request until it
- * can send tasks in return. Thus, when a worker receives a steal request whose
- * state is STATE_FAILED, the sender is either tree.left_child or
- * tree.right_child. At this point, there is a "lifeline" between parent and
- * child: the child will not send further steal requests (unless MAXSTEAL > 1)
+ * is then passed on to tree.parent as a work sharing request: the parent holds
+ * on to this request until it can send tasks in return. Thus, when a worker
+ * receives a steal request whose state is STATE_FAILED, the sender is either
+ * tree.left_child or tree.right_child. At this point, there is a "lifeline"
+ * between parent and child: the child will not send further steal requests
  * until it receives new work from its parent. We have switched from work
- * stealing to work sharing.  This also means that backing off from work
+ * stealing to work sharing. This also means that backing off from work
  * stealing by withdrawing a steal request for a short while is no longer
  * needed, as steal requests are withdrawn automatically.
  *
  * Termination occurs once worker 0 detects that both left and right subtrees
  * of workers are idle and worker 0 is itself idle.
  *
- * When a worker receives new work, it must check its "lifelines" and try to
- * distribute as many tasks as possible, thereby reactivating workers further
- * down in the tree.
+ * When a worker receives new work, it must check its "lifelines" (queue of
+ * work sharing requests) and try to distribute as many tasks as possible,
+ * thereby reactivating workers further down in the tree.
  */
 
 /*
  * Steal requests carry one of the following states:
- * - STATE_WORKING means the requesting worker is still busy
+ * - STATE_WORKING means the requesting worker is (likely) still busy
  * - STATE_IDLE means the requesting worker has run out of tasks
  * - STATE_FAILED means the requesting worker backs off and waits for tasks
  *   from its parent worker
@@ -1005,7 +1005,7 @@ static inline bool handle(struct steal_request *req)
 	return false;
 }
 
-// Handle as many steal requests in backoff_queue as possible; leave steal
+// Handle as many work sharing requests as possible; leave work sharing
 // requests that cannot be answered with tasks enqueued
 static inline void share_work(void)
 {
@@ -1021,7 +1021,7 @@ static inline void share_work(void)
 				assert(tree.right_subtree_is_idle);
 				tree.right_subtree_is_idle = false;
 			}
-			// Dequeue/Discard
+			// Dequeue/discard
 			(void)DEQUEUE_WORK_SHARING_REQUEST();
 		} else {
 			break;
